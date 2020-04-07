385eb67a47e39b6cc9e130d734a8845b8e52efbb
madoht <oht222@naver.com>
2020-04-07 22:58:22 +0900

    extracting titles & companies

--- indeed.py
+++ indeed.py
@@ -5,20 +5,28 @@ URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&a
 LIMIT = 50
 
 def extract_indeed_pages(): 
-    indeed_result = requests.get(URL)   
-    indeed_soup = BeautifulSoup(indeed_result.text, "html.parser") 
-    pagination = indeed_soup.find("div", {"class" : "pagination"}) 
+    result = requests.get(URL)   
+    soup = BeautifulSoup(result.text, "html.parser") 
+    pagination = soup.find("div", {"class" : "pagination"}) 
     links = pagination.find_all('a')   
     pages = []  
     for link in links[:-1]: 
         pages.append(int(link.string)) 
-
     max_pages = pages[-1]
     return max_pages
 
 def extract_indeed_jobs(last_page):
     jobs = []
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start = {page * LIMIT}")
-        print(result.status_code)
+    #for page in range(last_page):
+    result = requests.get(f"{URL}&start = {0 * LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        title = (result.find("div", {"class": "title"})).find("a")["title"]
+        company = result.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            print(str(company_anchor.string))
+        else:
+            print(str(company.string))
     return jobs
