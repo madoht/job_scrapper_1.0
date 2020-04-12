348b031a1d919ad6a093e9369b44397d0dd05a28
madoht <oht222@naver.com>
2020-04-13 00:37:20 +0900

    StackOverFlowFinish&CSV

--- so.py
+++ so.py
@@ -15,27 +15,26 @@ def get_last_page():
 def extract_job(html):
     title = html.find("h2", {"class": "mb4"}).find("a")["title"]
     company, location = html.find("h3", {"class": "mb4"}).find_all("span", recursive = False)
-    # company = company.get_text(strip=True)
-    # location = location.get_text(strip=True).strip("-").strip("\r").strip("\n")
-    # job_id = html['data-jobid']
-    print(company.string, location.string)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True).strip("-").strip("\r").strip("\n")
+    job_id = html['data-jobid']
     return {
-        # "title" : title
-        # 'company': company, 
-        # 'location': location, 
-        # "apply_link": f"https://stackoverflow.com/jobs/{job_id}"
+        'title': title,
+        'company': company, 
+        'location': location, 
+        "apply_link": f"https://stackoverflow.com/jobs/{job_id}"
         }
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping S0: Page: {page}")
         result = requests.get(f"{URL}&pg={page + 1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
             job = extract_job(result)
-            print(job)
             jobs.append(job)
     return jobs
 
